286aae1c629628bcf85c2dc255f58236ab939e06
Tomáš Roun <rountoma@fel.cvut.cz>
2016-06-14 19:04:01 +0200

    refactored export of the format function

--- format/format.js
+++ format/format.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = String.prototype.format = function format(...data) {
+module.exports = function format(...data) {
   let i = 0;
   return this.replace(/\{\}/g, function() {
     return data[i++];
